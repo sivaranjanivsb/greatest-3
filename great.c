94832e4ed061361704139d145da4c296cd641555
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2017-09-08 14:49:20 +0100

    Create great.c

---
+++ great.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+void main()
+{
+int a,b,c;
+if((a>b)&&(a>c))
+{
+printf("a is greatest");
+}
+elseif((b>c)&&(b>a))
+{
+printf("b is greater");
+}
+else
+{
+printf("c is greater");
+}
+}
